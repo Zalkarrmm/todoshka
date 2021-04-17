8931bb213190cc97af7e9dd2b307d7f853518960
Zalkarrmm <zalkarrmm@gmail.com>
2021-04-17 13:14:50 +0600

    newcomm

---
+++ js/auth.js
@@ -0,0 +1,33 @@
+const username = document.querySelector('.username');
+const password = document.querySelector('.password');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click' , e=> {
+    e.preventDefault();
+
+    if(username.value === '' && password.value === ''){
+        alert('Пустые поля!');
+    }else{
+        if(username.value === 'admin' && password.value === 'admin'){
+            alert('welcome');
+            window.open('todo.html' , '_self');
+            username.value = '';
+            password.value = '';
+            localStorage.setItem('isAuth' , 'true')
+
+        }else{
+            alert('wrong username or password!');
+            username.value ='';
+            password.value = '';
+            localStorage.setItem('isAuth' , 'false')
+        }
+    }
+})
+
+// check auth
+window.addEventListener('load' , () => {
+    const isAuth = localStorage.getItem('isAuth');
+
+    isAuth === 'true' ? window.open('todo.html' , '_self') : null;
+})
+
